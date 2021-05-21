fbf34317990917343f490725c1ccb54b797b4286
laryssacarraro <laryssacarraro@gmail.com>
2021-05-21 12:26:33 -0400

    Display the forecast data using the API

--- src/script.js
+++ src/script.js
@@ -47,7 +47,69 @@ let month = months[getMonth];
 let dateElement = document.querySelector("#now-date");
 dateElement.innerHTML = `${day} ${month} ${monthDate}, ${hour}:${minutes} `;
 
-// Search Weather
+// Display Forecast 
+
+function formatDay(timestamp) {
+
+  let date = new Date(timestamp * 1000);
+
+  let day = date.getDay();
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+  return days[day];
+}
+
+function displayForecast(response) {
+
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+  forecast.forEach(function(forecastDay, index) {
+    
+    if(index < 6) {
+forecastHTML = forecastHTML + `
+ 
+<div class="col-2">
+<div class="forecast-day">${formatDay(forecastDay.dt)}</div>
+<img src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png" alt="" width="50">
+<div class="forecast-temperature">
+<span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span> 
+ <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
+ </div>
+</div>`;
+
+}
+
+});
+  
+forecastHTML = forecastHTML + `</div>`;
+
+ forecastElement.innerHTML = forecastHTML;
+  
+}
+
+
+// Get Forecast
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+
+  let apiKey = "ca96a46b83f0206010d93234cc8d803f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayForecast);
+}
+
+
+// Display Weather
 
 function displayWeather(response) {
   document.querySelector("#city-h1").innerHTML = response.data.name;
@@ -68,6 +130,8 @@ function displayWeather(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 // Default city displayed
